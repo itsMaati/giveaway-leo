fb9a2bcb6fe648db32c3229e0ee7897cbc972d4b
itsmaati <alimm854@gmail.com>
2024-07-26 18:10:51 +0330

    first commit

---
+++ leo_giveaway/build/README.md
@@ -0,0 +1,13 @@
+# giveaway_v2_43643988.aleo
+
+## Build Guide
+
+To compile this Aleo program, run:
+```bash
+snarkvm build
+```
+
+To execute this Aleo program, run:
+```bash
+snarkvm run hello
+```
---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import aleoLogo from "./assets/aleo.svg";
+import "./App.css";
+import helloworld_program from "../helloworld/build/main.aleo?raw";
+import { AleoWorker } from "./workers/AleoWorker";
+import { WalletMultiButton } from "@demox-labs/aleo-wallet-adapter-reactui";
+import { useWallet } from '@demox-labs/aleo-wallet-adapter-react';
+import { Transaction, WalletAdapterNetwork,  WalletNotConnectedError,} from '@demox-labs/aleo-wallet-adapter-base';
+import "@demox-labs/aleo-wallet-adapter-reactui/styles.css";
+
+const aleoWorker = AleoWorker();
+function App() {
+  const { publicKey, requestTransaction, transactionStatus } = useWallet();
+  
+  const [winneraddr, setWinnerAddr] = useState('');
+
+  const CreateTransaction = async (method: string, values: Array<any>, success_message: string ) => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    const tx = Transaction.createTransaction(
+      publicKey,
+      WalletAdapterNetwork.TestnetBeta,
+      "giveaway_v2_43643988.aleo",
+      method,
+      values,
+      100000,
+      false,
+    );
+
+    if (requestTransaction) {
+      const result = await requestTransaction(tx);
+      console.log(result);
+
+      if (transactionStatus) {
+        const status = await transactionStatus(result);
+        console.log(status)
+        if(status == 'Completed'){
+          alert(success_message)
+        }
+      }
+    }
+  }
+
+
+  const changeWinnerAddr = (event) => {
+    setWinnerAddr(event.target.value);
+  };
+  const EnterGiveaway = async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+    await CreateTransaction('enter_giveaway',[publicKey],'you successfully participated in this giveaway with address: '+publicKey)
+  }
+
+  const Reset = async () => {
+    CreateTransaction('admin_reset',[],'Successfully Reset Stats');
+  }
+
+  const Initialize = async () => {
+    CreateTransaction('admin_reset',[],'Initialized');
+  }
+
+  const ChooseWinner = async () => {
+    console.log(winneraddr.toString());  
+    CreateTransaction('choose_winner',[winneraddr.toString()],'Winner was chosen as '+winneraddr);
+  }
+
+
+  return (
+    <div className="container">
+      <header className="navbar">
+        <WalletMultiButton />
+      </header>
+
+      <div className="title">
+        <h2>GiveAway App</h2>
+      </div>
+
+      <div className="sections">
+        <div className="section">
+          <p>Admin section</p>
+          <input type="text" id="winner_addr" placeholder="Enter the winner address" value={winneraddr} 
+        onChange={changeWinnerAddr}></input>
+          <button onClick={ChooseWinner}>Choose Winner</button>
+          <button onClick={Reset}>Reset</button>
+          <button onClick={Initialize}>Initialize</button>
+
+        </div>
+        <div className="section">
+          <p>User section</p>
+          <button onClick={EnterGiveaway}>Enter GiveAway</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
